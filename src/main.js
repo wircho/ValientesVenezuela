fff135656cb7c390864bb6f9a4625a35b2397859
Gaby <gabbysue90@gmail.com>
2017-05-04 01:04:42 -0400

    fonts

--- src/main.js
+++ src/main.js
@@ -101,7 +101,7 @@ const App = React.createClass({
         <h1>#VALIENTES</h1>
         <p>Una colecci&#243;n de afiches inspirados en los h&#233;roes venezolanos 
         <br/> que salen a luchar todos los d&#237;as por un cambio en nuestro pa&#237;s. 
-        <br/> Todos <a> #Valientes </a> <br/> <br/> 
+        <br/> Todos <a> #VALIENTES </a> <br/> <br/> 
          Descarga todos los afiches gratis, listos para imprimir.</p>
         <Posters/>
       </div>
