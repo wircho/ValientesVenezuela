b54fbdde9034701a2af8a85ba6a9ba95f90830d4
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2017-05-05 14:21:57 -0400

    added url loader packages for images

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
       }
     ]
   },
